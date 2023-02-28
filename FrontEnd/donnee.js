ab9f55a7271f3a6463c37d964bed98da28bb5b0c
Paul Leblanc <azikx4qg@gmail.com>
2023-02-28 18:07:54 +0100

    Ajout des filtres

--- FrontEnd/donnee.js
+++ FrontEnd/donnee.js
@@ -4,18 +4,133 @@ const imagesContainer = document.querySelector('.gallery')
 const reponse = fetch('http://localhost:5678/api/works')
     .then((reponse) => reponse.json())
     .then((data) => {
-        data.forEach((image) => {
+        data.forEach((work) => {
             const figure = document.createElement('figure')
             const figureCaption = document.createElement('figcaption')
             const figureImage = document.createElement('img')
 
-            figureImage.src = image.imageUrl
-            figureImage.alt = image.title
-            figureCaption.innerHTML = image.title
+            figureImage.src = work.imageUrl
+            figureImage.alt = work.title
+            figureCaption.innerHTML = work.title
+            figure.className = work.category.name
 
             imagesContainer.appendChild(figure)
             figure.appendChild(figureImage)
-            figure.appendChild(figureCaption)       
+            figure.appendChild(figureCaption)    
+            
+        });
     });
-});
     
+
+//Filtres//
+//Filtre Objets//
+        
+function filtreObjet(){
+    //afficher les Objets//
+    var elementObjets = document.querySelectorAll('.Objets');
+
+    for (var i = 0; i < elementObjets.length; i++){
+        elementObjets[i].style.display = 'block';
+    }
+
+    //cacher les autres travaux//
+    var elementsAppartements = document.querySelectorAll(".Appartements");
+    var elementsHotelRestaurants = document.querySelectorAll("[class*=Hotels][class*=restaurants]");
+
+    for (var i = 0; i < elementsAppartements.length; i++){
+        elementsAppartements[i].style.display = 'none';
+    }
+
+    for (var j = 0; j < elementsHotelRestaurants.length; j++){
+        elementsHotelRestaurants[j].style.display = 'none';
+    }
+}
+var bouton = document.getElementById('btnObjet');
+bouton.addEventListener('click',filtreObjet);
+              
+       
+//Filtre Hotel & resutaurants//
+        
+function filtreHotelsRestaurants(){
+    //afficher les Hotels & restaurants//
+    var elementsHotelRestaurants = document.querySelectorAll("[class*=Hotels][class*=restaurants]");
+
+    for (var i = 0; i < elementsHotelRestaurants.length; i++){
+        elementsHotelRestaurants[i].style.display = 'block';
+    }
+
+    //cacher  les autrex travaux//
+    var elementsAppartements = document.querySelectorAll('.Appartements');
+    var elementsObjets = document.querySelectorAll('.Objets');
+
+    for (var i = 0; i < elementsAppartements.length; i++){
+        elementsAppartements[i].style.display = 'none';
+    }
+
+    for (var j = 0; j < elementsObjets.length; j++){
+        elementsObjets[j].style.display = 'none';
+    }
+}
+
+var bouton = document.getElementById('btnHotelRestaurant');
+bouton.addEventListener('click',filtreHotelsRestaurants);
+
+        
+//Filtre Appartements//
+
+function filtreAppartements(){
+            
+    //afficher les Appartements//
+    var elementsAppartements = document.querySelectorAll('.Appartements');
+
+    for(var i = 0; i < elementsAppartements.length; i++){
+        elementsAppartements[i].style.display = 'block';
+    }
+
+    //cacher le autrex travaux//
+    var elementsObjets = document.querySelectorAll('.Objets');
+    var elementsHotelRestaurants = document.querySelectorAll("[class*=Hotels][class*=restaurants]");
+
+    for (var j = 0; j < elementsObjets.length; j++){
+        elementsObjets[j].style.display = 'none';
+    }
+
+    for(var j= 0; j < elementsHotelRestaurants.length; j++){
+        elementsHotelRestaurants[j].style.display = 'none';
+    }
+}
+
+var bouton = document.getElementById('btnAppartement');
+bouton.addEventListener('click',filtreAppartements);
+
+//Filtre Tous//
+
+function filtreTous(){
+    //Afficher tout les travaux//
+    var elementsObjets = document.querySelectorAll('.Objets');
+    var elementsAppartements = document.querySelectorAll('.Appartements');
+    var elementsHotelRestaurants = document.querySelectorAll("[class*=Hotels][class*=restaurants]");
+
+    for (var i = 0; i < elementsObjets.length; i++){
+        elementsObjets[i].style.display = 'block';
+    }
+
+    for(var i = 0; i < elementsAppartements.length; i++){
+        elementsAppartements[i].style.display = 'block';
+    }
+
+    for (var i = 0; i < elementsHotelRestaurants.length; i++){
+        elementsHotelRestaurants[i].style.display = 'block';
+    }
+}
+
+var bouton = document.getElementById('btnTous');
+bouton.addEventListener('click',filtreTous);
+
+
+
+
+    
+
+
+
